1975c82ee17166e4a1168a5828a1209b7545ce18
Jonathan Banda <bandajonathan07@gmail.com>
2023-07-06 18:55:22 -0500

    Today I put in some degree options so next time I can get class information for each of the majors.

--- DegreePlan/DegreePlan.cpp
+++ DegreePlan/DegreePlan.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
+
 
 int main() {
     cout << "\n";
@@ -17,13 +19,21 @@ int main() {
         cout << "1";
     }
     if (userChoice == 2) {
-        cout << "2";
+        cout << "\n";
+        cout << "Computer Science - 1\n";
+        cout << "Civil Engineer - 2\n";
+        cout << "Biology - 3\n";
+        cout << "Criminal Justice - 4\n";
+        cout << "Business - 5\n";
+        cout << "Enter choice: ";
+        cin >> userChoice;
+
     }
     if (userChoice == 3) {
         cout << "3";
     }
     if (userChoice == 4) {
-        cout << "4";
+        cout << "Goodbye!";
     }
 
     cout << "\n";
