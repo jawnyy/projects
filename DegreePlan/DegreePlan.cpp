2af56f18d7a541f01ef1d6a9b65cd565b35c9671
Jonathan Banda <bandajonathan07@gmail.com>
2023-07-09 20:16:53 -0500

    Added a Degree and Students class.

--- DegreePlan/DegreePlan.cpp
+++ DegreePlan/DegreePlan.cpp
@@ -1,10 +1,46 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <cstdlib>
 using namespace std;
+
+class Degree {
+    public:
+    void MajorChoice(int choice) {
+        if (choice == 1) {
+            cout << "\n";
+            cout << "You chose 1 PagMan\n"; // classes go here
+        }
+        else if (choice == 2) {
+            cout << "\n";
+            cout << "You chose 2 PagMan\n"; // classes go here
+        }
+        else if (choice == 3) {
+            cout << "\n";
+            cout << "You chose 3 PagMan\n"; // classes go here
+        }
+        else if (choice == 4) {
+            cout << "\n";
+            cout << "You chose 4 PagMan\n"; // classes go here
+        }
+        else if (choice == 5) {
+            cout << "\n";
+            cout << "You chose 5 PagMan\n"; // classes go here
+        }
+    }
+};
+
+class Student {
+    public:
+    //not really sure just make so student info can be obtained
+};
 
 
 int main() {
+    Degree degree;
+    Student student;
+
+
     cout << "\n";
     cout << "Menu:\n";
     cout << "Student Information - 1\n";
@@ -27,7 +63,7 @@ int main() {
         cout << "Business - 5\n";
         cout << "Enter choice: ";
         cin >> userChoice;
-
+        degree.MajorChoice(userChoice);
     }
     if (userChoice == 3) {
         cout << "3";
